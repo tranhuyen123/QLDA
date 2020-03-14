e81402f9431e8a619f56a1c05a797c3ef7f2cc92
tranhuyen123 <59789766+tranhuyen123@users.noreply.github.com>
2020-03-14 16:53:15 +0700

    Create baitoanhoanvi.cpp

---
+++ baitoanhoanvi.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#define n 5
+using namespace std;
+int a[n];
+void xuat(){
+	for(int i=0;i<n;i++)
+		cout<<a[i];
+		cout<<endl;
+}
+void Try(int i){
+	for(int j=0;j<=1;j++)
+	{
+		a[i]=j;
+		if(i==n-1) xuat();
+		else Try(i+1);
+	}
+}
+
+int main(){
+	Try(0);
+}
+	
+
